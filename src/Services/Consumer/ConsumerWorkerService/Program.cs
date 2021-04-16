842cd2d6865133f1f3fb8887cd174447fe937c01
Saban Durna <sabandurna@gmail.com>
2021-04-16 02:40:24 +0200

    Implement and configure Nlog Add Windows service support Add systemd support for working on linux

--- src/Services/Consumer/ConsumerWorkerService/Program.cs
+++ src/Services/Consumer/ConsumerWorkerService/Program.cs
@@ -1,9 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLog.Web;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ConsumerWorkerService
 {
@@ -11,14 +9,33 @@ namespace ConsumerWorkerService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .UseSystemd()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
-                });
+                })
+                .UseNLog();
     }
 }
